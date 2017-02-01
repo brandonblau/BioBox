96eae9e1eae41a5abc62bc121bac0f914c3eb392
Brandon Blau <bblau@lion.lmu.edu>
2017-02-01 12:07:01 -0800

    imgProc.py modification for terminal response string.

--- imgProc.py
+++ imgProc.py
@@ -11,10 +11,11 @@ xwidth = width/square;
 ywidth = height/square;
 img_copy = Image("/var/www/html/cam.jpg");
 
-print img.size();
-
 peaks = img.huePeaks();
 hue = img.hueDistance(peaks[0][0]);
+
+good=0;
+bad=0;
 
 for n in range(0, square*square):
     x = n % square;
@@ -23,21 +24,25 @@ for n in range(0, square*square):
     newlayer = DrawingLayer(img.size());
     
     assignedColor = Color.GREEN;
+    good = good +1;
     blue, red, green = crop.meanColor();
     avg = (red+blue+green)/3;
     if avg > 100:
         assignedColor = Color.RED;
+        good = good - 1;
+        bad = bad + 1;
     img_copy.dl().rectangle((x*xwidth,y*ywidth), (xwidth,ywidth), color = assignedColor, filled=True, alpha = 150);
 
     #img_copy.show()
     #time.sleep(0.1);
 
-#img_copy.show();
-#time.sleep(0.3);
-
+print('There are '+str(good)+' good divisions and '+str(bad)+' bad divisions');
+avg2 = (good /(good+bad) ) 
+print('giving '+str( avg2 )+'%');
 img_copy.show();
 time.sleep(2);
-print peaks;
+
+
 
 hue.show();
 time.sleep(4);
